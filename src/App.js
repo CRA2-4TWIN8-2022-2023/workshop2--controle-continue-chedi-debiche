581ea6683de2c2564f31f5f88f3288602762629c
chedi-debiche <chedi.debiche@esprit.tn>
2023-02-02 19:53:30 +0100

    rendu2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 
+const option = (
+  <ol>
+    <li>TWIN</li>
+    <li>BI</li>
+    <li>SAE</li>
+
+  </ol>
+);
+
+let x="Bonjour";
+var y="tout"
+const z="le monde!"
+
+const expression = <p> 1 + 1 = { 1 + 1 } </p>
+
+var nom=<h2>chedi</h2>
+var prenom=<h2>debiche</h2>
+const photo = <img src="image.jpg" alt ="maPhoto" width="500px" height="500px"/>;
+
+const user={prenom:'chedi',nom:'debiche'};
+
+function formatName(user){
+  return user.prenom + '--' + user.nom;
+}
+
+
+var myStyle = {
+  fontSize:100,
+  color:'#FF0000'
+}
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,9 +49,32 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
+ATELIER 2
+
+{option}
+
+{x} {y} {z}
+
+{expression}
+<div style={myStyle}>{nom}</div>
+<div style={myStyle}>{prenom}</div>
+{photo}
+<br></br>
+<br></br>
+
+Bonjour : {user.prenom}--{user.nom}
+
+<br></br>
+
+{formatName(user)}
+
+
+
     </div>
   );
 }
+
 
 export default App;
